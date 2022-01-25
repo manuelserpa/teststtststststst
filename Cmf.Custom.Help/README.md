5fb6708c4facd279cde4b8473f0780665f43e34a
Manuel Brandão <ManuelBrandao@criticalmanufacturing.com>
2022-01-25 19:53:32 +0000

    Help and Html

---
+++ Cmf.Custom.Help/README.md
@@ -0,0 +1,37 @@
+CMF MES HTML Starter Repository
+========= 
+
+**html-starter** is the base of any CMF MES HTML Graphical User Interface customization project.
+This repository is meant to be a quick and easy way of getting started with CMF MES GUI customization, by providing a basic structure for the project and all the recommended tooling.
+
+## Before you start
+
+Make sure:
+* node@10.x+ and npm@6.x+ are installed;
+* You have access to the Critical Manufacturing Setup with the version of the HTML5 you want to customize;
+
+
+## Getting Started
+
+1. Pre-install the following npm packages globally:
+
+```
+npm install -g gulp
+npm install -g yo
+```
+
+2. Clone this repository into your repository root.
+
+3. Edit the package.json file and replace the name and description of the package.
+
+4. Run:
+
+```
+npm install
+```
+
+## Additional Information
+
+This package was developed during the [UX-FAB: Universal Experience for Advanced Fabs](http://www.criticalmanufacturing.com/en/r-d/ux-fab) project.
+
+![Portugal2020](http://www.criticalmanufacturing.com/uploads/richtext/images/2017030610420258bd3cfa033c0.png)
---
+++ Cmf.Custom.Help/gulpfile.js
@@ -0,0 +1,127 @@
+var gulp = require('gulp'),
+  rootUtils = require("@criticalmanufacturing/dev-tasks/root.main"),
+  pluginRunSequence = require('run-sequence'),
+  pluginYargs = require('yargs').argv,
+  _config = require('./.dev-tasks.json'),
+  _packagesPath = './src/packages/', //note the ending /
+  _dependenciesPath = './dependencies/',
+  _framework = _config.framework,
+  //NOTE: order matters in these arrays!
+  _dependencies = _config.dependencies,
+  _packages = _config.packages,
+  _apps = [`${_config.webAppPrefix}.web`],  
+  tasks = null,
+  applyOps = null;
+
+if (typeof _framework === "string" && _framework !== "") {
+  tasks = require("./src/" + _framework + "/gulpfile.js")(gulp, _framework);
+}
+_dependencies.forEach(function (dep) {
+  require(_dependenciesPath + dep + "/gulpfile.js")(gulp, dep);
+});
+_packages.forEach(function (pkg) {
+  require(_packagesPath + pkg + "/gulpfile.js")(gulp, pkg);
+});
+require(`./apps/${_config.webAppPrefix}.web/gulpfile.js`)(gulp, `${_config.webAppPrefix}.web`);
+
+applyOps = function (actions) {
+  if (!Array.isArray(actions)) { actions = [actions]; }
+  var operations = [];
+  var dependencyOperations = [];
+  var packageOperations = [];
+  _dependencies.forEach(function (dep) {
+    actions.forEach(function (action) {
+      operations.push(dep + ">" + action);
+    });
+  });
+
+  actions.forEach(function (action) {
+    if (typeof _framework === "string" && _framework !== "") {
+      operations.push(_framework+ '>' + action);
+    }
+  })
+
+  _packages.forEach(function (mod) {
+    actions.forEach(function (action) {
+      operations.push(mod + ">" + action)
+    });
+  });
+  
+  return operations;
+};
+
+/*
+ * Build all
+ */
+gulp.task('build', function(callback) { 
+  
+  if(pluginYargs.server) {
+    var isWebAppCompilable = true;
+    rootUtils.runOperation(__dirname, _dependencies, _framework, _packages, _apps, "build", callback, typeof _framework === "string" && _framework !== "", isWebAppCompilable);
+
+  }else{
+    var ops = applyOps('build');
+
+    // On customized projects we would only require to compile the web if the project defined a framework on their own
+    var isWebAppCompilable = _config.isWebAppCompilable;
+    if (isWebAppCompilable === true) {
+      ops = ops.concat(`${_config.webAppPrefix}.web>build`);
+    }
+
+    if (ops.length > 0) {
+      ops = ops.concat(callback);
+      pluginRunSequence.apply(this, ops);
+    } else {
+      callback();
+    }
+  }
+  
+});
+
+/*
+ * Install all
+ */
+gulp.task('install', function (callback) {
+
+  if(pluginYargs.server) {
+    rootUtils.runOperation(__dirname, _dependencies, _framework, _packages, _apps, "install", callback, typeof _framework === "string" && _framework !== "", true);
+
+  }else{
+    var ops = applyOps(['install']);
+
+    ops.unshift(`${_config.webAppPrefix}.web>install`);
+
+    ops = ops.concat(callback);
+    pluginRunSequence.apply(this, ops);
+  }
+});
+
+/*
+ * Install and build apps
+ */
+gulp.task('apps', function (callback) { pluginRunSequence.apply(this, ['build', `${_config.webAppPrefix}.web>build`, callback]); });
+
+/*
+ * start serving the web app
+ */
+gulp.task('start', function (callback) { pluginRunSequence(`${_config.webAppPrefix}.web>start`); });
+
+/*
+ * start running the tests
+ */
+gulp.task('test', function (callback) { pluginRunSequence(`${_config.webAppPrefix}.web>test`); });
+
+/*
+ * clean the workspace
+ */
+gulp.task('clean', function (callback) { pluginRunSequence.apply(this, applyOps('clean').concat(callback)); })
+
+gulp.task('clean-cmflibs', function (callback) { pluginRunSequence.apply(this, applyOps('clean-cmflibs').concat(callback)); })
+
+gulp.task('purge', function (callback) { pluginRunSequence.apply(this, applyOps('purge').concat(callback)); })
+
+gulp.task('watch', function (callback) { pluginRunSequence.apply(this, applyOps('watch').concat(callback)); });
+
+gulp.task('create-missing-i18n', function (callback) { pluginRunSequence.apply(this, applyOps('create-missing-i18n').concat(callback)); });
+
+gulp.task('ci:publish', function (callback) { pluginRunSequence.apply(this, applyOps('ci:publish').concat(callback)); });
