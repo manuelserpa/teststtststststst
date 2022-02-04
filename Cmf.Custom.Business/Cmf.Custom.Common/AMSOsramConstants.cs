42379aeb6e58768a8c1d13ca789db8faaa0ccef0
David Guilherme <DavidGuilherme@criticalmanufacturing.com>
2022-02-04 10:20:43 +0000

    Changed Resolve Name Generator #152687 #156892

--- Cmf.Custom.Business/Cmf.Custom.Common/AMSOsramConstants.cs
+++ Cmf.Custom.Business/Cmf.Custom.Common/AMSOsramConstants.cs
@@ -2,5 +2,13 @@
 {
     public class AMSOsramConstants
     {
+        #region Name Generators
+
+        /// <summary>
+        /// Production lot Name Generator
+        /// </summary>
+        public const string CustomGenerateProductionLotNames = "CustomGenerateProductionLotNames";
+
+        #endregion  
     }
 }
