a1d20f510cfc80b19109723a0b0d99d21b26ceb9
André Cruz <AndreCruz@criticalmanufacturing.com>
2022-02-04 10:10:42 +0000

    Split Lot Name Generator #152688 #156910

--- Cmf.Custom.Business/Cmf.Custom.Common/AMSOsramConstants.cs
+++ Cmf.Custom.Business/Cmf.Custom.Common/AMSOsramConstants.cs
@@ -2,5 +2,10 @@
 {
     public class AMSOsramConstants
     {
+        #region Name Generators
+
+        public const string CustomGenerateSplitLotNames = "CustomGenerateSplitLotNames";
+
+        #endregion
     }
 }
