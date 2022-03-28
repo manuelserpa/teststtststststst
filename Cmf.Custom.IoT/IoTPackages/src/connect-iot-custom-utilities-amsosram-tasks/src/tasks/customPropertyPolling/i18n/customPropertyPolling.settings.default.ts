6d8d387593fb185815efb78ceecfd27a107516ba
Rodrigo Azevedo <RodrigoAzevedo@criticalmanufacturing.com>
2022-03-28 02:15:28 +0100

    data trace and poling

---
+++ Cmf.Custom.IoT/IoTPackages/src/connect-iot-custom-utilities-amsosram-tasks/src/tasks/customPropertyPolling/i18n/customPropertyPolling.settings.default.ts
@@ -0,0 +1,21 @@
+import i18nControls from "cmf.core.controls/src/i18n/main.default";
+
+export default {
+    TITLE: "Custom Pooling Data",
+    OUTPUT: i18nControls.labels.OUTPUT,
+    OUTPUTS: i18nControls.labels.OUTPUTS,
+    INVALID_OUTPUTS: "Some outputs are invalid",
+    REPEATED_OUTPUTS: "Some outputs are repeated",
+    PROPERTIES: "Equipment Property",
+    OUTPUT_TYPE: "Output type",
+    NOT_SPECIFIED: "Not specified",
+    PROPERTY_NOT_READABLE: "Property is not defined as readable",
+    NAME: i18nControls.labels.NAME,
+    TYPE: i18nControls.labels.TYPE,
+    SETTINGS: "Settings",
+    ALLOW_NON_READABLE: "Allow non-readable properties",
+    AUTO_ACTIVATE: "Auto activate",
+    NUMBER_OF_OCCURRENCES: "Number of Occurrences",
+    INTERVAL: "Interval",
+    EMIT_IN_NEW_CONTEXT: "Emit in new context"
+}
