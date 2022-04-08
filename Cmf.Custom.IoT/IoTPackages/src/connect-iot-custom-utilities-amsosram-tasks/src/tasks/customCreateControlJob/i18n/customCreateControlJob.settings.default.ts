9c48858f87c23c88e0919a41b6ee6258dd793ee2
Rafael Wieler <RafaelWieler@criticalmanufacturing.com>
2022-04-08 12:02:11 +0100

    Changes in the string name

--- Cmf.Custom.IoT/IoTPackages/src/connect-iot-custom-utilities-amsosram-tasks/src/tasks/customCreateControlJob/i18n/customCreateControlJob.settings.default.ts
+++ Cmf.Custom.IoT/IoTPackages/src/connect-iot-custom-utilities-amsosram-tasks/src/tasks/customCreateControlJob/i18n/customCreateControlJob.settings.default.ts
@@ -2,5 +2,5 @@ export default {
     SETTINGS: "Settings",
     OBJECT_SPEC: "ObjSpec",
     OCCUPIED_SLOT: "Occupied Slot For Map Carrier",
-    USE_CARRIER_AT_LOAD_PORT: "Use Standard String as 'CarrierAtLoadPort${LoadPort}' for the ContainerName"
+    USE_CARRIER_AT_LOAD_PORT: "Use 'CarrierAtLoadPort${LP}' for the ContainerName"
 };
--- Cmf.Custom.IoT/IoTPackages/src/connect-iot-custom-utilities-amsosram-tasks/src/tasks/customMultiCreateProcessJob/i18n/customMultiCreateProcessJob.settings.default.ts
+++ Cmf.Custom.IoT/IoTPackages/src/connect-iot-custom-utilities-amsosram-tasks/src/tasks/customMultiCreateProcessJob/i18n/customMultiCreateProcessJob.settings.default.ts
@@ -4,5 +4,5 @@ export default {
     SEND_CARRIER_CONTENT: "Send Carrier Content",
     RECIPE_SPECIFICATION_TYPE: "Recipe Specification Type",
     OCCUPIED_SLOT: "Occupied Slot For Map Carrier",
-    USE_CARRIER_AT_LOAD_PORT: "Use Standard String as 'CarrierAtLoadPort${LoadPort}' for the ContainerName"
+    USE_CARRIER_AT_LOAD_PORT: "Use 'CarrierAtLoadPort${LP}' for the ContainerName"
 };
