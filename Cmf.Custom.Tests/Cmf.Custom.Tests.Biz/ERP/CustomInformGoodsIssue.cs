d5aa18233b3d7ae9e94c315b38a2fe92e728346f
David Guilherme <DavidGuilherme@criticalmanufacturing.com>
2022-04-08 10:24:44 +0100

    Test Setup

---
+++ Cmf.Custom.Tests/Cmf.Custom.Tests.Biz/ERP/CustomInformGoodsIssue.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cmf.Custom.Tests.Biz.ERP
+{
+    [TestClass]
+    public class CustomInformGoodsIssue
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <TestCaseID>CustomInformGoodsIssue.CustomInformGoodsIssue_HappyPath</TestCaseID>
+        /// <Author>David Guilherme</Author>
+        [TestMethod]
+        public void CustomInformGoodsIssue_HappyPath()
+        {
+
+        }
+    }
+}
