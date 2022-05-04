a01c7d90bad70b32ab304e3aad88c10f83f4f6a1
Rodrigo Azevedo <RodrigoAzevedo@criticalmanufacturing.com>
2022-05-04 09:52:38 +0100

    adding reader test

---
+++ Cmf.Custom.Tests/Cmf.Custom.Tests.IoT/Tests/HermosLFM4xReader/HermosLFM4xReader.cs
@@ -0,0 +1,32 @@
+﻿using cmConnect.TestFramework.Environment;
+using cmConnect.TestFramework.EquipmentSimulator.Drivers;
+using Cmf.Navigo.BusinessObjects;
+using Cmf.SECS.Driver;
+using AMSOsramEIAutomaticTests.Objects.Extensions;
+using System;
+using System.Net;
+using AMSOsramEIAutomaticTests;
+using System.Collections.Generic;
+
+namespace Cmf.Custom.Tests.IoT.Tests.HermosLFM4xReader
+{
+    class HermosLFM4xReader : CommonTests
+	{
+         public Dictionary<String, String> PortId = new Dictionary<string, string>();
+
+	     public void TestInit()
+         {
+            base.Equipment.RegisterOnMessage("S18F9", OnS18F9);
+
+         }
+
+        protected bool OnS18F9(SecsMessage request, SecsMessage reply)
+        {
+            var readerId = request.Item.GetChildList()[0];
+
+            reply.Item.Clear();
+            
+            return true;
+        }
+    }
+}
