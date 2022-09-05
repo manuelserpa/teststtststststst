9c7142a78eb66d9dee19ea70d2e937a06ee61f7f
André Cruz <AndreCruz@criticalmanufacturing.com>
2022-09-05 19:05:53 +0100

    Create constants for GT columns and Tibco Properties. Fix way to send Text Messages to Tibco. Set JMS_TIBCO_COMPRESS only if CompressMessage is defined on GT with value "true".

--- Cmf.Custom.TibcoEMS/Cmf.Custom.TibcoEMS.ServiceManager/Common/TibcoEMSConstansts.cs
+++ Cmf.Custom.TibcoEMS/Cmf.Custom.TibcoEMS.ServiceManager/Common/TibcoEMSConstansts.cs
@@ -5,9 +5,44 @@
         #region Generic Table
 
         /// <summary>
-        /// CustomTibcoEMSGatewayResolver
+        /// Custom TibcoEMS Gateway Resolver table name
         /// </summary>
-        public const string GTCustomTibcoEMSGatewayResolver = "CustomTibcoEMSGatewayResolver";
+        public const string GenericTableCustomTibcoEMSGatewayResolver = "CustomTibcoEMSGatewayResolver";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table Subject Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverSubjectProperty = "Subject";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table Topic Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverTopicProperty = "Topic";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table Rule Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverRuleProperty = "Rule";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table IsEnabled Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverIsEnabledProperty = "IsEnabled";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table QueueMessage Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverQueueMessageProperty = "QueueMessage";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table TextMessage Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverTextMessageProperty = "TextMessage";
+
+        /// <summary>
+        /// Custom TibcoEMS Gateway Resolver table CompressMessage Property 
+        /// </summary>
+        public const string GenericTableCustomTibcoEMSGatewayResolverCompressMessageProperty = "CompressMessage";
 
         #endregion
 
@@ -35,6 +70,20 @@
         /// DEE Action: CustomGetTibcoConfigurations
         /// </summary>
         public const string CustomGetTibcoConfigurations = "CustomGetTibcoConfigurations";
+
+        #endregion
+
+        #region TibcoEMS Properties
+
+        /// <summary>
+        /// Property: TibcoEMSPropertyCompressTextMessage
+        /// </summary>
+        public const string TibcoEMSPropertyCompressTextMessage = "JMS_TIBCO_COMPRESS";
+
+        /// <summary>
+        /// Property: TibcoEMSPropertyMapMessageField
+        /// </summary>
+        public const string TibcoEMSPropertyMapMessageField = "field";
 
         #endregion
     }
