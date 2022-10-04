38827ed49c476bafa5f86a7a95ec757d54332bc9
Gabor Balogh <gabor.balogh@glaub.hu>
2022-10-04 15:23:22 +0200

    Added new project to the Business solution for the ERP communication external services, modified the CustomReportToERPItem class to have all the requested properties, added the beginning of the message sending to CustomSendProcessMessage, added process rule to update the IntegrationHandlerResolution ST

---
+++ Cmf.Custom.Data/DEEs/ProcessRules/2.0.0/After/CustomUpdateIntegrationHandlerResolutionSmartTable.cs
@@ -0,0 +1,63 @@
+﻿using Cmf.Custom.amsOSRAM.Common;
+using Cmf.Foundation.BusinessObjects;
+using Cmf.Foundation.BusinessObjects.Abstractions;
+using Cmf.Foundation.BusinessObjects.SmartTables;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Cmf.Custom.amsOSRAM.Actions.ProcessRules._2._0._0.After
+{
+    internal class CustomUpdateIntegrationHandlerResolutionSmartTable : DeeDevBase
+    {
+        public override bool DeeTestCondition(Dictionary<string, object> Input)
+        {
+            //---Start DEE Condition Code---
+
+            return true;
+
+            //---End DEE Condition Code---
+        }
+
+        public override Dictionary<string, object> DeeActionCode(Dictionary<string, object> Input)
+        {
+            //---Start DEE Code---
+
+            //System
+            UseReference("%MicrosoftNetPath%System.Data.Common.dll", "System.Data");
+
+            //Foundation
+            UseReference("Cmf.Foundation.BusinessObjects.dll", "Cmf.Foundation.BusinessObjects.SmartTables");
+
+            //Custom
+            UseReference("Cmf.Custom.amsOSRAM.Common.dll", "Cmf.Custom.amsOSRAM.Common");
+
+            // Load Smart Table
+            ISmartTable integrationHandlerResolution = new SmartTable() { Name = "IntegrationHandlerResolution"};
+
+            integrationHandlerResolution.Load();
+            integrationHandlerResolution.LoadData();
+
+            // Get the rwo we want to modifí from the Smart Table
+            INgpDataRow resolveRow = new NgpDataRow
+            {
+                { "FromSystem", "MES" },
+                { "ToSystem", "ERP" },
+                { "MessageType", amsOSRAMConstants.CustomPerformConsumption }
+            };
+
+            INgpDataSet resolveDataSet = integrationHandlerResolution.Resolve(resolveRow, false);
+            DataSet dataSet = NgpDataSet.ToDataSet(resolveDataSet);
+
+            // Modify row
+            dataSet.Tables[0].Rows[0]["ActionName"] = "CustomSendProcessMessage";
+            dataSet.AcceptChanges();
+
+            // Update the Smart Table
+            integrationHandlerResolution.InsertOrUpdateRows(NgpDataSet.FromDataSet(dataSet));
+
+            //---End DEE Code---
+
+            return Input;
+        }
+    }
+}
