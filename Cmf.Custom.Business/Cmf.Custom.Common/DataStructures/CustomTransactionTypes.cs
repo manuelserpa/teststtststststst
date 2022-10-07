1e52297681b998094641a7f02b12a9c93f6ed150
André Cruz <AndreCruz@criticalmanufacturing.com>
2022-10-07 12:21:59 +0100

    Action to send Material message

---
+++ Cmf.Custom.Business/Cmf.Custom.Common/DataStructures/CustomTransactionTypes.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.Serialization;
+
+namespace Cmf.Custom.amsOSRAM.Common.DataStructures
+{
+    [DataContract]
+    public enum CustomTransactionTypes
+    {
+        [EnumMember]
+        MaterialCreate,
+
+        [EnumMember]
+        MaterialTerminate,
+
+        [EnumMember]
+        MaterialDispatch,
+
+        [EnumMember]
+        MaterialTrackIn,
+
+        [EnumMember]
+        MaterialTrackOut,
+
+        [EnumMember]
+        MaterialMoveNext,
+
+        [EnumMember]
+        MaterialSplit,
+
+        [EnumMember]
+        MaterialMerge,
+
+        [EnumMember]
+        MaterialLoss,
+
+        [EnumMember]
+        MaterialBonus,
+
+        [EnumMember]
+        MaterialHold,
+
+        [EnumMember]
+        MaterialRelease,
+
+        [EnumMember]
+        ContainerAssociation
+    }
+}
---
+++ Cmf.Custom.Data/DEEs/Tibco/CustomSendEventMessage.cs
@@ -0,0 +1,397 @@
+﻿using Cmf.Common.CustomActionUtilities;
+using Cmf.Custom.amsOSRAM.Common;
+using Cmf.Custom.amsOSRAM.Common.DataStructures;
+using Cmf.Custom.amsOSRAM.Common.Extensions;
+using Cmf.Foundation.BusinessObjects.GenericTables;
+using Cmf.Foundation.Common;
+using Cmf.Foundation.Common.Abstractions;
+using Cmf.Navigo.BusinessObjects.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Stimulsoft.Report.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Cmf.Custom.amsOSRAM.Actions.Tibco
+{
+    public class CustomSendEventMessage : DeeDevBase
+    {
+        public override bool DeeTestCondition(Dictionary<string, object> Input)
+        {
+            //---Start DEE Condition Code---   
+
+            #region Info
+            /// <summary>
+            /// Summary text
+            ///     
+            /// Action Groups:
+            /// Depends On:
+            /// Is Dependency For:
+            /// Exceptions:
+            /// </summary>
+            #endregion
+
+            bool canExecute = false;
+
+            if (Input.TryGetValueAs("ActionGroupName", out string actionGroupName) && !string.IsNullOrWhiteSpace(actionGroupName))
+            {
+                Dictionary<string, CustomTransactionTypes> associatedTransactions = new Dictionary<string, CustomTransactionTypes>()
+                {
+                    { "BusinessObjects.MaterialCollection.Create.Post", CustomTransactionTypes.MaterialCreate },
+                    { "BusinessObjects.MaterialCollection.Terminate.Post", CustomTransactionTypes.MaterialTerminate },
+                    { "BusinessObjects.MaterialCollection.Dispatch.Post", CustomTransactionTypes.MaterialDispatch },
+                    { "BusinessObjects.MaterialCollection.TrackIn.Post", CustomTransactionTypes.MaterialTrackIn },
+                    { "BusinessObjects.MaterialCollection.TrackOut.Post", CustomTransactionTypes.MaterialTrackOut },
+                    { "BusinessObjects.MaterialCollection.MoveToNextStep.Post", CustomTransactionTypes.MaterialMoveNext },
+                    { "BusinessObjects.MaterialCollection.Split.Post", CustomTransactionTypes.MaterialSplit },
+                    { "BusinessObjects.MaterialCollection.RecordLoss.Post", CustomTransactionTypes.MaterialLoss },
+                    { "BusinessObjects.MaterialCollection.RecordBonus.Post", CustomTransactionTypes.MaterialBonus },
+                    { "BusinessObjects.MaterialCollection.Hold.Post", CustomTransactionTypes.MaterialHold },
+                    { "BusinessObjects.Material.Merge.Post", CustomTransactionTypes.MaterialMerge },
+                    { "BusinessObjects.Material.Release.Post", CustomTransactionTypes.MaterialRelease },
+                    { "BusinessObjects.Container.AssociateMaterials.Post", CustomTransactionTypes.ContainerAssociation }
+                };
+
+                if (associatedTransactions.ContainsKey(actionGroupName))
+                {
+                    CustomTransactionTypes transactionToExecute = associatedTransactions[actionGroupName];
+
+                    GenericTable customTransactionsToTibcoGT = new GenericTable() { Name = amsOSRAMConstants.GenericTableCustomTransactionsToTibco };
+                    customTransactionsToTibcoGT.Load();
+
+                    customTransactionsToTibcoGT.LoadData(new Foundation.BusinessObjects.QueryObject.FilterCollection()
+                    {
+                        new Foundation.BusinessObjects.QueryObject.Filter()
+                        {
+                            Name = amsOSRAMConstants.GenericTableCustomTransactionsToTibcoTransactionProperty,
+                            Operator = FieldOperator.IsEqualTo,
+                            LogicalOperator = LogicalOperator.AND,
+                            Value = transactionToExecute
+                        },
+                        new Foundation.BusinessObjects.QueryObject.Filter()
+                        {
+                            Name = amsOSRAMConstants.GenericTableCustomTransactionsToTibcoIsEnabledProperty,
+                            Operator = FieldOperator.IsEqualTo,
+                            LogicalOperator = LogicalOperator.AND,
+                            Value = true
+                        }
+                    });
+
+                    if (customTransactionsToTibcoGT.HasData)
+                    {
+                        canExecute = true;
+
+                        DeeContextHelper.SetContextParameter("TransactionToExecute", transactionToExecute);
+                    }
+                }
+            }
+
+            return canExecute;
+
+            //---End DEE Condition Code---
+        }
+
+        public override Dictionary<string, object> DeeActionCode(Dictionary<string, object> Input)
+        {
+            //---Start DEE Code---     
+
+            //Foundation
+            UseReference("", "Cmf.Foundation.Common.Exceptions");
+
+            // Get services provider information
+            IServiceProvider serviceProvider = (IServiceProvider)Input["ServiceProvider"];
+            IEntityFactory entityFactory = serviceProvider.GetService<IEntityFactory>();
+
+            string messageSubject = string.Empty;
+            string messageXml = string.Empty;
+            IMaterialCollection materialCollection = entityFactory.CreateCollection<IMaterialCollection>();
+
+            CustomTransactionTypes transactionToExecute = (CustomTransactionTypes)DeeContextHelper.GetContextParameter("TransactionToExecute");
+            switch (transactionToExecute)
+            {
+                case CustomTransactionTypes.MaterialCreate:
+                case CustomTransactionTypes.MaterialTerminate:
+                case CustomTransactionTypes.MaterialDispatch:
+                case CustomTransactionTypes.MaterialTrackIn:
+                case CustomTransactionTypes.MaterialTrackOut:
+                case CustomTransactionTypes.MaterialMoveNext:
+                case CustomTransactionTypes.MaterialSplit:
+                case CustomTransactionTypes.MaterialLoss:
+                case CustomTransactionTypes.MaterialBonus:
+                case CustomTransactionTypes.MaterialHold:
+
+                    materialCollection = DeeActionHelper.GetInputItem<IMaterialCollection>(Input, Navigo.Common.Constants.MaterialCollection);
+
+                    if (materialCollection.Any())
+                    {
+                        messageSubject = amsOSRAMConstants.CustomLotChange;
+                        messageXml = GetMaterialCollectionXml(materialCollection, transactionToExecute.ToString());
+                    }
+
+                    break;
+
+                case CustomTransactionTypes.MaterialRelease:
+                case CustomTransactionTypes.MaterialMerge:
+
+                    IMaterial material = DeeActionHelper.GetInputItem<IMaterial>(Input, Navigo.Common.Constants.Material);
+
+                    if (material is not null)
+                    {
+                        materialCollection.Add(material);
+
+                        messageSubject = amsOSRAMConstants.CustomLotChange;
+                        messageXml = GetMaterialCollectionXml(materialCollection, transactionToExecute.ToString());
+                    }
+
+                    break;
+
+                case CustomTransactionTypes.ContainerAssociation:
+
+                    messageSubject = amsOSRAMConstants.CustomEquipmentStatusChange;
+
+                    break;
+            }
+
+            if (!string.IsNullOrWhiteSpace(messageSubject) && !string.IsNullOrWhiteSpace(messageXml))
+            {
+                // Publish message on Message Bus
+                Utilities.PublishTransactionalMessage(messageSubject,
+                                                      JsonConvert.SerializeObject(new
+                                                      {
+                                                          Header = GetMessageHeader(materialCollection, transactionToExecute.ToString()),
+                                                          Message = messageXml
+                                                      }));
+            }
+
+            string GetMaterialCollectionXml(IMaterialCollection materials, string actionName)
+            {
+                // List of ElementNames to discard on output XML
+                List<string> xmlElementsToDiscard = new List<string>()
+                {
+                    "CurrentBOMAssemblyType",
+                    "CurrentBOMVersion",
+                    "LineFlowVersion",
+                    "LineValidationMode",
+                    "LineAssemblyMode",
+                    "InhibitMoveFromStep",
+                    "OverrideProductBlock",
+                    "InhibitShip",
+                    "InTransitFromState",
+                    "InTransitType",
+                    "IsInNonSequentialBlock",
+                    "LocationAltitude",
+                    "LocationLatitude",
+                    "LocationLongitude",
+                    "ResourceAssociationType",
+                    "SplitMergeRestrictionType",
+                    "NotificationCount",
+                    "TimeConstraintsCount",
+                    "CurrentSamplingPattern",
+                    "SamplingSequence",
+                    "RequiredFutureAction",
+                    "MaterialTransferFromFacility",
+                    "MaterialTransferCostCenter",
+                    "MaximumAssembleDate",
+                    "CurrentBOMTrackOutLossesMode",
+                    "MaintenanceHoldCount",
+                    "AccountsToProductionOrderQuantity",
+                    "ExcludeFromScheduling",
+                    "HasFromDependencies",
+                    "HasToDependencies",
+                    "CurrentBOMWeighAndDispenseMode",
+                    "ExperimentMaterialGroupName",
+                    "ExperimentSubMaterialNumber",
+                    "TargetMaterialQuantity",
+                    "TargetMaterialUnits",
+                    "CurrentBOMUnits",
+                    "PickListItemCount",
+                    "IsInTransferOrderItem",
+                    "MoistureSensitivityLevel",
+                    "FloorLifeOpenDate",
+                    "FloorLifeCounterState",
+                    "FloorLifeRemainingHours",
+                    "FloorLifeSealed",
+                    "ManufacturerPartNumber",
+                    "ManufacturerLotNumber",
+                    "DateCode",
+                    "CapacityClass",
+                    "IsRoHSCompliant",
+                    "IsApproved",
+                    "RequiredResource",
+                    "OpenInspectionOrderCount",
+                    "OpenInspectionOrderStepSampleCount",
+                    "PendingLineReworkReturn",
+                    "InTransitToFacility",
+                    "IsDispatchable",
+                    "Priority",
+                    "RequiredService",
+                    "ShippingLabel",
+                    "SynchronizeMapUnits",
+                    "MasterMap",
+                    "IsTemplate",
+                    "RelationCollection",
+                    "DocumentationURL",
+                    "Image",
+                    "DataGroupName"
+                };
+
+                // List of AttributeNames associated to the Elements to discard on output XML
+                List<string> xmlAttributesToDiscard = new List<string>()
+                {
+                    "type",
+                    "actualtype",
+                    "ExportId",
+                    "RefId"
+                };
+
+                // Load exported XML object
+                XDocument xDocument = XDocument.Parse(materials.ExportToString());
+
+                #region Get & Replace Attributes associated to the Materials
+
+                Dictionary<string, XElement> materialsAttributes = ExportMaterialsAttributesToXml(materials);
+
+                if (materialsAttributes is not null && materialsAttributes.Any())
+                {
+                    foreach (KeyValuePair<string, XElement> materialAttributes in materialsAttributes)
+                    {
+                        // Material Element filtered by MaterialName
+                        XElement materialElement = xDocument.Root.Descendants("Object")?.Where(obj => obj.Attribute("type").Value.StartsWith("Cmf.Navigo.BusinessObjects.Material", StringComparison.InvariantCultureIgnoreCase))?
+                                                                                        .Where(e => e.Element("Name").Attribute("value").Value.Equals(materialAttributes.Key, StringComparison.InvariantCultureIgnoreCase))?
+                                                                                        .Single();
+
+                        if (materialElement is not null && materialAttributes.Value is not null)
+                        {
+                            // Replace the Attributes element associated to the Material
+                            materialElement.Element("Attributes").ReplaceWith(materialAttributes.Value);
+                        }
+                    }
+                }
+
+                #endregion Get & Replace Attributes associated to the Materials
+
+                #region Remove discarded Elements & Attributes from XML
+
+                // Remove Material discarded Elements
+                xDocument.Root.Descendants().Where(e => xmlElementsToDiscard.Contains(e.Name.LocalName)).Remove();
+
+                // Remove Material discarded Attributes
+                xDocument.Root.Descendants().Attributes().Where(a => xmlAttributesToDiscard.Contains(a.Name.LocalName)).Remove();
+
+                #endregion Remove discarded Elements & Attributes from XML
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.LoadXml(xDocument.ToString());
+
+                return xmlDocument.InnerXml;
+            }
+
+            Dictionary<string, XElement> ExportMaterialsAttributesToXml(IMaterialCollection materials, Dictionary<string, XElement> materialsAttributes = null)
+            {
+                Dictionary<string, XElement> outputMaterialsAttributes = new Dictionary<string, XElement>();
+
+                if (materialsAttributes is not null && materialsAttributes.Any())
+                {
+                    outputMaterialsAttributes = materialsAttributes;
+                }
+
+                foreach (IMaterial material in materials)
+                {
+                    material.LoadAttributes();
+
+                    if (material.Attributes.Count > 0)
+                    {
+                        XElement materialAttributesElement = new XElement("Attributes");
+
+                        List<XElement> attributes = new List<XElement>();
+
+                        foreach (KeyValuePair<string, object> attribute in material.Attributes)
+                        {
+                            XElement attributeElement = new XElement(attribute.Key, new XAttribute("value", attribute.Value.ToString()));
+
+                            attributes.Add(attributeElement);
+                        }
+
+                        materialAttributesElement.Add(attributes);
+
+                        outputMaterialsAttributes.Add(material.Name, materialAttributesElement);
+                    }
+
+                    if (material.SubMaterialCount > 0)
+                    {
+                        material.LoadChildren();
+
+                        ExportMaterialsAttributesToXml(material.SubMaterials, outputMaterialsAttributes);
+                    }
+                }
+
+                return outputMaterialsAttributes;
+            }
+
+            object GetMessageHeader(IMaterialCollection materials, string action)
+            {
+                string lotName = string.Empty;
+                string pathFrom = string.Empty;
+                string pathTo = string.Empty;
+                string sAPproductType = string.Empty;
+
+                IMaterial material = entityFactory.Create<IMaterial>();
+
+                if (materials.Any())
+                {
+                    if (materials.FirstOrDefault().ParentMaterial is not null)
+                    {
+                        materials.FirstOrDefault().LoadTopMost();
+                        material = materials.FirstOrDefault().TopMostMaterial;
+                    }
+                    else
+                    {
+                        material = materials.FirstOrDefault();
+                    }
+
+                    lotName = material.Name;
+
+                    string siteCode = string.Empty;
+                    material.Facility.Site.Load();
+                    if (material.Facility.Site.HasAttribute(amsOSRAMConstants.CustomSiteCodeAttribute, true))
+                    {
+                        siteCode = material.Facility.Site.GetAttributeValue(amsOSRAMConstants.CustomSiteCodeAttribute) as string;
+                    }
+
+                    //TO: Get FacilityCode
+
+                    string stepLogicalName = string.Empty;
+                    if (material.Step.ContainsLogicalNames)
+                    {
+                        stepLogicalName = material.Step.LogicalNames.FirstOrDefault().Name;
+                    }
+
+                    pathFrom = pathTo = $"{siteCode}.{string.Empty}.{stepLogicalName}";
+
+                    if (material.Product.HasRelatedAttribute(amsOSRAMConstants.ProductAttributeSAPProductType, true))
+                    {
+                        sAPproductType = material.Product.GetRelatedAttributeValue(amsOSRAMConstants.ProductAttributeSAPProductType) as string;
+                    }
+                }
+
+                return new
+                {
+                    stdObjectName = lotName,
+                    stdFrom = pathFrom,
+                    stdTo = pathTo,
+                    stdProductType = sAPproductType,
+                    stdDataOrigin = Environment.MachineName,
+                    stdTransaction = action
+                };
+            }
+
+            //---End DEE Code---
+
+            return Input;
+        }
+    }
+}
