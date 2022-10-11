d2db8ce9d91331be12411ed747a02c07580aa2b4
André Cruz <AndreCruz@criticalmanufacturing.com>
2022-10-11 18:15:15 +0100

    Goods Issues tests

--- Cmf.Custom.Tests/Cmf.Custom.Tests.Biz.Common/ERP/GoodsIssue/CustomReportToERPItem.cs
+++ Cmf.Custom.Tests/Cmf.Custom.Tests.Biz.Common/ERP/GoodsIssue/CustomReportToERPItem.cs
@@ -6,66 +6,91 @@ namespace Cmf.Custom.Tests.Biz.Common.ERP.GoodsIssue
     /// <summary>
     /// Information to be sent for movement type 261
     /// </summary>
+    [XmlRoot(ElementName = "ReportToERPItem")]
     public class CustomReportToERPItem
     {
         /// <summary>
         /// The CreatedOn property
         /// </summary>
-        [XmlElement(ElementName = "TIME_STAMP")]
-        public DateTime CreatedOn { get; set; }
+        [XmlElement(ElementName = "id")]
+        public string Id { get; set; }
 
         /// <summary>
-        /// The ProductionOrderNumber property
+        ///  The Batch property
         /// </summary>
-        [XmlElement(ElementName = "PROD_ORDER_NR")]
-        public string ProductionOrderNumber { get; set; }
+        [XmlElement(ElementName = "Batch")]
+        public string BatchName { get; set; }
+
+        /// <summary>
+        /// The CostCenter property
+        /// </summary>
+        [XmlElement(ElementName = "CostCenter")]
+        public string CostCenter { get; set; }
 
         /// <summary>
         /// The MaterialName property
         /// </summary>
-        [XmlElement(ElementName = "LOT_NUMBER")]
+        [XmlElement(ElementName = "LotNumber")]
         public string MaterialName { get; set; }
 
         /// <summary>
-        /// The ProductName property
+        /// The ProductionOrderNumber property
         /// </summary>
-        [XmlElement(ElementName = "MATERIAL_NR")]
-        public string ProductName { get; set; }
+        [XmlElement(ElementName = "ProductionOrderNr")]
+        public string ProductionOrderNumber { get; set; }
 
         /// <summary>
         /// The Quantity property
         /// </summary>
-        [XmlElement(ElementName = "QUANTITY")]
-        public decimal? Quantity { get; set; }
+        [XmlElement(ElementName = "Quantity")]
+        public string Quantity { get; set; }
 
         /// <summary>
         /// The Units property
         /// </summary>
-        [XmlElement(ElementName = "QTY_UNIT")]
+        [XmlElement(ElementName = "QuantityUnit")]
         public string Units { get; set; }
-
-        /// <summary>
-        /// The SAPStore property
-        /// </summary>
-        [XmlElement(ElementName = "SAP_STORE")]
-        public string SAPStore { get; set; }
-
-        /// <summary>
-        /// The Site property
-        /// </summary>
-        [XmlElement(ElementName = "SITE")]
-        public string Site { get; set; }
 
         /// <summary>
         /// The MovementType property
         /// </summary>
-        [XmlElement(ElementName = "MOVE_TYPE")]
+        [XmlElement(ElementName = "MovementType")]
         public string MovementType { get; set; }
 
         /// <summary>
-        /// The SubMaterialCount property
+        /// The Site property
         /// </summary>
-        [XmlElement(ElementName = "WAFERS")]
-        public int SubMaterialCount { get; set; }
+        [XmlElement(ElementName = "Site")]
+        public string Site { get; set; }
+
+        /// <summary>
+        /// The SAPStore property
+        /// </summary>
+        [XmlElement(ElementName = "SapStore")]
+        public string SAPStore { get; set; }
+
+        /// <summary>
+        /// The SapToStore property
+        /// </summary>
+        [XmlElement(ElementName = "SapToStore")]
+        public string SAPToStore { get; set; }
+
+        /// <summary>
+        /// The MatCalYear property
+        /// </summary>
+        [XmlElement(ElementName = "MatCalYear")]
+        public string MatCalYear { get; set; }
+
+        /// <summary>
+        /// The ProductName property
+        /// </summary>
+        [XmlElement(ElementName = "MaterialNr")]
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// The MatRecNr property
+        /// </summary>
+        [XmlElement(ElementName = "MatRecNr")]
+        public string MatRecNr { get; set; }
     }
 }
